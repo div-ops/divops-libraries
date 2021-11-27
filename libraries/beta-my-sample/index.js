af8129d8c23ac062199b42fa0ac35f9f4b5ea189
CRECO <creaticoding@gmail.com>
2021-11-27 17:16:04 +0900

    feat(ci): simple ci 구성 [skip ci] (#1)
    * Test
    
    * test
    
    * Test
    
    * test
    
    * [skip ci] version up
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ libraries/beta-my-sample/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+function default_1() {
+    var A = "1 v5";
+    console.log("hello this is my sample libraries!" + A);
+}
+exports["default"] = default_1;
--- libraries/scaffolder/index.ts
+++ libraries/scaffolder/index.ts
@@ -1,3 +1,3 @@
 export default function () {
-  console.log("this is scaffolder");
+  console.log("this is scaffolder v2");
 }
