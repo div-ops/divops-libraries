0e4997b1a6fc00742ac6d2d45e69dc6e4c3df96f
creaticoding <creaticoding@gmail.com>
2022-03-17 00:36:41 +0900

    fix: notion-database reduce의 의도치 않은 버그 수정

--- libraries/notion-databsae/src/utils/reduce-concat.ts
+++ libraries/notion-databsae/src/utils/reduce-concat.ts
@@ -1,13 +1,19 @@
-export function reduceConcat(acc = "", cur, key) {
+export function reduceConcat(
+  previousValue = "",
+  currentValue: string,
+  _currentIndex: number,
+  _array: string[],
+  key?: string
+): string {
   if (key == null) {
-    return `${acc + cur}`;
+    return `${previousValue}${currentValue}`;
   }
 
-  if (cur == null || cur[key] == null) {
+  if (currentValue == null || currentValue[key] == null) {
     throw new Error(
-      `[reduceConcat] cur[key]가 올바르지 않습니다. (cur: ${cur}, key: ${key})`
+      `[reduceConcat] cur[key]가 올바르지 않습니다. (currentValue: ${currentValue}, key: ${key})`
     );
   }
 
-  return `${acc + cur[key]}`;
+  return `${previousValue}${currentValue[key]}`;
 }
