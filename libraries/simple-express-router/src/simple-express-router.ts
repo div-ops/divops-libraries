ffb59100fc44573238671f2a3bb512bb31129b2d
seokho-jeong <seokho.jeong@toss.im>
2022-03-28 00:24:12 +0900

    fix required simple-express-router

--- libraries/simple-express-router/src/simple-express-router.ts
+++ libraries/simple-express-router/src/simple-express-router.ts
@@ -2,8 +2,13 @@ import { Request, Response, Router } from "express";
 
 type Resource = (...args) => Promise<any>;
 
-const createSimpleRouter = (resource: Resource) => {
+const createSimpleRouter = (resource: Resource, requiredMiddleware?: any) => {
   const router = Router();
+
+  // FIXME: any type
+  if (requiredMiddleware != null) {
+    router.use(requiredMiddleware);
+  }
 
   router.use(async (req: Request, res: Response) => {
     const args = { ...req.query, ...req.body, ...req.params };
