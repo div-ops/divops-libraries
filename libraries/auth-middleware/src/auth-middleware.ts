e5316538351a2fff165dcaf8d774cfee134c1efa
seokho-jeong <seokho.jeong@toss.im>
2022-03-27 22:18:27 +0900

    fix: parameter

--- libraries/auth-middleware/src/auth-middleware.ts
+++ libraries/auth-middleware/src/auth-middleware.ts
@@ -14,16 +14,12 @@ export function authMiddleware(key, validator: (req, res) => Promise<void>) {
   };
 }
 
-export function requireAuthMiddleware(
-  key,
-  validator: (req, res) => Promise<void>
-) {
+export function requireAuthMiddleware(validator: (req, res) => Promise<void>) {
   return async (req, res, next) => {
     try {
       await validator(req, res);
       return next();
     } catch {
-      req.app.set(key, null);
       return res.status(403).send("403 Forbidden");
     }
   };
