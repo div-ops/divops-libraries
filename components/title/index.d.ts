3b6c39985002d6ce261bee03655699f8cdd3feb1
seokho-jeong <seokho.jeong@toss.im>
2022-04-03 05:52:48 +0900

    remove: index.d.ts

--- components/title/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export default function Title({ padding, className, underline, children, }: {
-    padding?: string;
-    className?: string;
-    underline?: boolean;
-    children: string | string[] | JSX.Element | JSX.Element[] | undefined;
-}): JSX.Element;
