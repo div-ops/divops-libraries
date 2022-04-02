8483244d330a01a1f5ec27d5785fcaf4bf68ea51
seokho-jeong <seokho.jeong@toss.im>
2022-04-03 05:08:53 +0900

    fix: typescript up and yarn up and fix title component

---
+++ components/title/src/index.tsx
@@ -0,0 +1,23 @@
+export default function Title({
+  padding,
+  className = "",
+  underline = false,
+  children,
+}: {
+  padding?: string;
+  className?: string;
+  underline?: boolean;
+  children: string | string[] | JSX.Element | JSX.Element[] | undefined;
+}) {
+  return (
+    <h1
+      className={className}
+      style={{
+        ...(padding ? { padding: `${padding};` } : {}),
+        ...(underline ? { textDecoration: `underline` } : {}),
+      }}
+    >
+      {children}
+    </h1>
+  );
+}
