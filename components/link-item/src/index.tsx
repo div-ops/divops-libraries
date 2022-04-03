0c7d0815f4d69639f06986ddcbd5b1143f9dffb9
seokho-jeong <seokho.jeong@toss.im>
2022-04-03 11:40:34 +0900

    fix: 외부 주입하여 처리

--- components/link-item/src/index.tsx
+++ components/link-item/src/index.tsx
@@ -1,36 +1,23 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useCallback } from "react";
+import { NextRouter } from "next/router";
 
 export default function LinkItem({
   className,
   href,
   target = "_self",
   children,
+  router,
 }: {
   className?: string;
   href: string;
   target?: "_self" | "_parent" | "_top" | "_blank";
   children: JSX.Element;
+  router: NextRouter;
 }) {
-  const router = useRouter();
-
-  const handleClick = useCallback(
-    (e: React.MouseEvent<HTMLElement>) => {
-      e.preventDefault();
-      if (router == null) {
-        console.log("useRouter", useRouter);
-        console.log("router", router);
-      }
-
-      if (router.isReady) {
-        router.push(href);
-      } else {
-        alert("잠시 후 재시도해주세요 🙏");
-      }
-    },
-    [router]
-  );
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    router.push(href);
+  };
 
   return (
     <li className={className}>
