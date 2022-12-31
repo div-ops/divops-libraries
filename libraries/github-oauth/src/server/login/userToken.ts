4bda3b22a25832b5361615682806171dad11987f
정석호 <seokho.jeong@toss.im>
2022-12-31 17:10:18 +0900

    fix

--- libraries/github-oauth/src/server/login/userToken.ts
+++ libraries/github-oauth/src/server/login/userToken.ts
@@ -1,5 +1,5 @@
-import { createGitHubOAuth } from "../createGitHubOAuth";
-import { NextApiRequest, NextApiResponse } from "../types";
+import { createGitHubOAuth } from "../../createGitHubOAuth";
+import { NextApiRequest, NextApiResponse } from "../../types";
 
 export function createUserToken({ name }: { name: string }) {
   return async function loginUserTokenAPI(
