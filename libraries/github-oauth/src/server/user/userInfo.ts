82de1c908a5bf4ac5df1e28a0693380df7f9a571
정석호 <seokho.jeong@toss.im>
2023-01-01 04:36:11 +0900

    fix

--- libraries/github-oauth/src/server/user/userInfo.ts
+++ libraries/github-oauth/src/server/user/userInfo.ts
@@ -1,18 +1,32 @@
 import { createGitHubOAuth } from "../../createGitHubOAuth";
 import { NextApiRequest, NextApiResponse } from "../../types";
 
+const cache: Record<string, any> = {};
+
 export function createUserInfo({ name }: { name: string }) {
   return async function userInfo(req: NextApiRequest, res: NextApiResponse) {
     try {
       const { authorization } = req.cookies;
       const gitHubOAuth = createGitHubOAuth({ name });
+      const promised = gitHubOAuth
+        .fetchUserInfo({
+          cryptedGitHubID: decodeURIComponent(authorization),
+        })
+        .then((x) => {
+          cache[decodeURIComponent(authorization)] = x;
+          return x;
+        });
+
+      if (cache[decodeURIComponent(authorization)] != null) {
+        return res.json({
+          data: cache[decodeURIComponent(authorization)],
+        });
+      }
 
       // const decoded = Buffer.from(authorization, "base64").toString("utf8");
 
       return res.json({
-        data: await gitHubOAuth.fetchUserInfo({
-          cryptedGitHubID: decodeURIComponent(authorization),
-        }),
+        data: await promised,
       });
     } catch (error: any) {
       return res.json({
