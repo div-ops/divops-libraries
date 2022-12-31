ab55c5f3e25b7844a8959a73a5c4a997ba5092b5
정석호 <seokho.jeong@toss.im>
2022-12-31 17:34:37 +0900

    fix(github-oauth): 필요없는 부분 제거

--- libraries/github-oauth/src/server/index.ts
+++ libraries/github-oauth/src/server/index.ts
@@ -1,16 +1,10 @@
-import {
-  createUserToken,
-  createCallback,
-  SetCookieOptions,
-  createSetCookie,
-} from "./login";
+import { createUserToken, SetCookieOptions, createSetCookie } from "./login";
 import { createUserInfo } from "./user";
 
 export const API = {
   of({ name }: { name: string }) {
     return {
       UserToken: () => createUserToken({ name }),
-      Callback: () => createCallback({ name }),
       SetCookie: (options: SetCookieOptions) => createSetCookie(options),
       UserInfo: () => createUserInfo({ name }),
     };
--- libraries/github-oauth/src/server/login/index.ts
+++ libraries/github-oauth/src/server/login/index.ts
@@ -1,3 +1,2 @@
-export * from "./callback";
 export * from "./setCookie";
 export * from "./userToken";
