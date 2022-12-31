35b3d87f3a7731e9dc75e78f86605728b623a5b3
정석호 <seokho.jeong@toss.im>
2022-12-31 17:16:07 +0900

    fix(github-oauth): add userInfo test API

--- libraries/github-oauth/src/server/index.ts
+++ libraries/github-oauth/src/server/index.ts
@@ -1,2 +1,18 @@
-export * from "./login";
-export * from "./user";
+import {
+  createUserToken,
+  createCallback,
+  SetCookieOptions,
+  createSetCookie,
+} from "./login";
+import { createUserInfo } from "./user";
+
+export const API = {
+  of({ name }: { name: string }) {
+    return {
+      UserToken: () => createUserToken({ name }),
+      Callback: () => createCallback({ name }),
+      SetCookie: (options: SetCookieOptions) => createSetCookie(options),
+      UserInfo: () => createUserInfo({ name }),
+    };
+  },
+};
