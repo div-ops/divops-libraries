441c64a44c1aba4881712413e3912315b85522de
정석호 <seokho.jeong@toss.im>
2023-01-01 04:41:49 +0900

    fix

--- libraries/github-oauth/src/server/user/userInfo.ts
+++ libraries/github-oauth/src/server/user/userInfo.ts
@@ -5,8 +5,12 @@ const cache: Record<string, any> = {};
 
 export function createUserInfo({ name }: { name: string }) {
   return async function userInfo(req: NextApiRequest, res: NextApiResponse) {
+    if (req.cookies.authorization == null) {
+      return res.json({ data: null });
+    }
+
     try {
-      const { authorization } = req.cookies;
+      const { authorization } = req.cookies ?? {};
       const gitHubOAuth = createGitHubOAuth({ name });
       const promised = gitHubOAuth
         .fetchUserInfo({
