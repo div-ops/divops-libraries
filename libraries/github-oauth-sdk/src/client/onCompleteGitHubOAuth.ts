6476bbd17a3e658d784f74e7b5f1dc64d3a7a137
정석호 <seokho.jeong@toss.im>
2022-12-31 17:56:03 +0900

    fix

--- libraries/github-oauth-sdk/src/client/onCompleteGitHubOAuth.ts
+++ libraries/github-oauth-sdk/src/client/onCompleteGitHubOAuth.ts
@@ -25,12 +25,13 @@ export async function onCompleteGitHubOAuth({
   });
 
   const Authorization = response.headers.get("Authorization");
+  const [, token] = Authorization.split(" ");
 
   const referrer = localStorage.getItem("referrer");
 
   localStorage.removeItem("referrer");
 
-  if (!Authorization || !referrer) {
+  if (!token || !referrer) {
     alert("잘못된 접근인데, 어떻게 오셨어요? 다시 접근해보세용!");
     setTimeout(() => {
       window.history.back();
@@ -40,11 +41,11 @@ export async function onCompleteGitHubOAuth({
 
   if (referrer.includes("?")) {
     window.location.assign(
-      `${referrer}${`&code=${window.btoa(encodeURIComponent(Authorization))}`}`
+      `${referrer}${`&code=${window.btoa(encodeURIComponent(token))}`}`
     );
   } else {
     window.location.assign(
-      `${referrer}${`?code=${window.btoa(encodeURIComponent(Authorization))}`}`
+      `${referrer}${`?code=${window.btoa(encodeURIComponent(token))}`}`
     );
   }
 }
