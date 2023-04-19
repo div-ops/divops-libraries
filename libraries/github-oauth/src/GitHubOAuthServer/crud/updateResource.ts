e94e452c76296737758b259bc32985353f219951
정석호 <seokho.jeong@toss.im>
2023-04-19 22:56:28 +0900

    feat: logging timestamp for create and update (3)

--- libraries/github-oauth/src/GitHubOAuthServer/crud/updateResource.ts
+++ libraries/github-oauth/src/GitHubOAuthServer/crud/updateResource.ts
@@ -12,9 +12,9 @@ interface Options extends CommonAPIOptions, CorsOptions {}
 export function createUpdateResource({ server, client, before }: Options) {
   return async function updateResource(req: Req, res: Res) {
     const start = Date.now();
-    console.log(`[UreateResource] 1 STEP: ${start + Date.now()}ms`);
+    console.log(`[UreateResource] 1 STEP: ${Date.now() - start}ms`);
     await before(req, res);
-    console.log(`[UreateResource] 2 STEP: ${start + Date.now()}ms`);
+    console.log(`[UreateResource] 2 STEP: ${Date.now() - start}ms`);
 
     try {
       const model = req.body.model;
@@ -23,23 +23,23 @@ export function createUpdateResource({ server, client, before }: Options) {
       const summary = req.body.summary;
 
       const cryptedGitHubId = getAuthorization(req);
-      console.log(`[UreateResource] 3 STEP: ${start + Date.now()}ms`);
+      console.log(`[UreateResource] 3 STEP: ${Date.now() - start}ms`);
 
       if (cryptedGitHubId == null) {
         return res.json({ data: null });
       }
 
       const gitHubOAuth = createGitHubOAuth({ server, client });
-      console.log(`[UreateResource] 4 STEP: ${start + Date.now()}ms`);
+      console.log(`[UreateResource] 4 STEP: ${Date.now() - start}ms`);
       const githubId = gitHubOAuth.decryptGitHubID({ cryptedGitHubId });
-      console.log(`[UreateResource] 5 STEP: ${start + Date.now()}ms`);
+      console.log(`[UreateResource] 5 STEP: ${Date.now() - start}ms`);
 
       if (githubId == null) {
         return res.status(400).json({
           message: `${githubId}에게 ${id}의 권한이 없습니다.`,
         });
       }
-      console.log(`[UreateResource] 6 STEP: ${start + Date.now()}ms`);
+      console.log(`[UreateResource] 6 STEP: ${Date.now() - start}ms`);
 
       await gitHubOAuth.updateResource({
         id,
@@ -49,7 +49,7 @@ export function createUpdateResource({ server, client, before }: Options) {
         resource,
         githubId,
       });
-      console.log(`[UreateResource] 7 STEP: ${start + Date.now()}ms`);
+      console.log(`[UreateResource] 7 STEP: ${Date.now() - start}ms`);
 
       return res.json({ status: "ok" });
     } catch (error: any) {
